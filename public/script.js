537fc942a4c1c5f1600b653e165b78ca413dd3a8
samitkapoor <samitkapoor77@gmail.com>
2023-04-04 01:32:36 +0530

    Added tools col on hovering

--- public/script.js
+++ public/script.js
@@ -1,7 +1,33 @@
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
+const shapesRow = document.getElementById("shapes-row");
+const brushesRow = document.getElementById("brushes-row");
+
+const brushesIcon = document.getElementById("brushes-row-icon");
+const toolsIcon = document.getElementById("tools-icon");
+
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   menu.classList.toggle("flex");
+});
+
+brushesIcon.addEventListener("mouseover", () => {
+  brushesRow.classList.remove("hidden");
+  brushesRow.classList.add("flex");
+});
+
+brushesIcon.addEventListener("mouseleave", () => {
+  brushesRow.classList.remove("flex");
+  brushesRow.classList.add("hidden");
+});
+
+toolsIcon.addEventListener("mouseover", () => {
+  shapesRow.classList.remove("hidden");
+  shapesRow.classList.add("flex");
+});
+
+toolsIcon.addEventListener("mouseleave", () => {
+  shapesRow.classList.remove("flex");
+  shapesRow.classList.add("hidden");
 });
